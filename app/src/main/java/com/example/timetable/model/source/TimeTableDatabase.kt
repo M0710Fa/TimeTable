ed52a66a0d41387a18826af5c81880c6b4d75278
M0710Fa <b1018188.fun@gmail.com>
2023-02-21 20:14:47 +0900

    databaseファイルの内容を追加

--- app/src/main/java/com/example/timetable/model/source/TimeTableDatabase.kt
+++ app/src/main/java/com/example/timetable/model/source/TimeTableDatabase.kt
@@ -1,8 +1,30 @@
 package com.example.timetable.model.source
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Subject::class], version = 1)
 abstract class TimeTableDatabase: RoomDatabase() {
+    abstract fun timeTableDao() : TimeTableDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TimeTableDatabase? = null
+
+        fun getDatabase(context: Context): TimeTableDatabase{
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TimeTableDatabase::class.java,
+                    "timetable_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
