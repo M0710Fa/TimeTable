e95d8d412c48e3788ac9fe30af287ab0c13cdd9e
M0710Fa <b1018188.fun@gmail.com>
2023-02-21 20:03:18 +0900

    Daoの追加

---
+++ app/src/main/java/com/example/timetable/model/source/TimeTableDao.kt
@@ -0,0 +1,24 @@
+package com.example.timetable.model.source
+
+import androidx.room.*
+
+@Dao
+interface TimeTableDao {
+    @Insert
+    suspend fun insertSubject(subject: Subject)
+
+    @Update
+    suspend fun updateSubject(subject: Subject)
+
+    @Delete
+    suspend fun deleteSubject(subject: Subject)
+
+    @Query("SELECT * FROM subjects WHERE id = :key")
+    suspend fun getSubject(id: Int):Subject
+
+    @Query("SELECT * FROM subjects")
+    suspend fun getAllSubjects(): List<Subject>
+
+    @Query("DELETE FROM subjects")
+    suspend fun deleteAllSubject()
+}
