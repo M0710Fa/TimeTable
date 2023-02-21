e9fe4620c09109731b3a565f088e43835288f036
M0710Fa <b1018188.fun@gmail.com>
2023-02-21 19:52:15 +0900

    データベースクラスの追加

---
+++ app/src/main/java/com/example/timetable/model/source/TimeTableDatabase.kt
@@ -0,0 +1,8 @@
+package com.example.timetable.model.source
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Subject::class], version = 1)
+abstract class TimeTableDatabase: RoomDatabase() {
+}
