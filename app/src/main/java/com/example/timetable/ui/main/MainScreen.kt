46e645b3b37ccfdb7426bfef836e2294d2b19e00
M0710Fa <b1018188.fun@gmail.com>
2023-02-22 01:39:28 +0900

    科目登録部分のUIの作成

---
+++ app/src/main/java/com/example/timetable/ui/main/MainScreen.kt
@@ -0,0 +1,82 @@
+package com.example.timetable.ui.main
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.timetable.R
+
+@Composable
+fun MainScreen(
+    modifier: Modifier = Modifier,
+    viewModel: MainViewModel = hiltViewModel(),
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    AddSubjectScreen(
+        subjectName = uiState.subjectName,
+        classRoom = uiState.classRoom,
+        teacher = uiState.teacher,
+        updateSubjectName = viewModel::updateSubjectName,
+        updateClassRoom = viewModel::updateClassRoom,
+        updateTeacher = viewModel::updateTeacher,
+    )
+}
+
+@Composable
+fun AddSubjectScreen(
+    modifier: Modifier = Modifier,
+    subjectName: String,
+    classRoom: String,
+    teacher: String,
+    updateSubjectName:(String) -> Unit,
+    updateClassRoom:(String) -> Unit,
+    updateTeacher:(String) -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text(text = "科目登録")
+        OutlinedTextField(
+            value = subjectName,
+            onValueChange = { updateSubjectName(it) },
+            label = {
+                Text(text = stringResource(id = R.string.subject_name))
+            },
+            modifier = modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(8.dp)
+        )
+        OutlinedTextField(
+            value = classRoom,
+            onValueChange = { updateClassRoom(it) },
+            label = {
+                Text(text = stringResource(id = R.string.class_room))
+            },
+            modifier = modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(8.dp)
+        )
+        OutlinedTextField(
+            value = teacher,
+            onValueChange = { updateTeacher(it) },
+            label = {
+                Text(text = stringResource(id = R.string.teacher))
+            },
+            modifier = modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(8.dp)
+        )
+    }
+}
---
+++ app/src/main/java/com/example/timetable/ui/main/MainViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.timetable.ui.main
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+data class MainUiState(
+    val subjectName: String = "",
+    val classRoom: String = "",
+    val teacher:String = ""
+)
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+): ViewModel() {
+    private val _uiState = MutableStateFlow(MainUiState())
+    val uiState: StateFlow<MainUiState> = _uiState
+
+    fun updateSubjectName(newSubjectName: String){
+        _uiState.update {
+            it.copy(subjectName = newSubjectName)
+        }
+    }
+
+    fun updateClassRoom(newClassRoom: String){
+        _uiState.update {
+            it.copy(classRoom = newClassRoom)
+        }
+    }
+
+    fun updateTeacher(newTeacher: String){
+        _uiState.update {
+            it.copy(teacher = newTeacher)
+        }
+    }
+
+}
