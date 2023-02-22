5e6be2a2743768edd2328df1b6b8403a9078bfff
M0710Fa <b1018188.fun@gmail.com>
2023-02-22 16:09:42 +0900

    DatabaseをDIする

--- app/src/main/java/com/example/timetable/model/repository/TimeTableRepositoryImpl.kt
+++ app/src/main/java/com/example/timetable/model/repository/TimeTableRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package com.example.timetable.model.repository
 
+import com.example.timetable.model.source.TimeTableDao
 import javax.inject.Inject
 
 class TimeTableRepositoryImpl @Inject constructor(
-
+    private val timeTableDao: TimeTableDao,
 ): TimeTableRepository {
+
 }
---
+++ app/src/main/java/com/example/timetable/model/source/TimeTableDatabaseModule.kt
@@ -0,0 +1,24 @@
+package com.example.timetable.model.source
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TimeTableDatabaseModule {
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context): TimeTableDatabase{
+        return Room.databaseBuilder(
+            context = context,
+            klass = TimeTableDatabase::class.java,
+            name = "timetable_database"
+        ).build()
+    }
+}
