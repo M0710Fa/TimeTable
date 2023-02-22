180af06803142165daebd87bce45dca545ec7727
M0710Fa <b1018188.fun@gmail.com>
2023-02-22 18:55:08 +0900

    repositoryに科目追加と参照用のメソッドを追加

--- app/src/main/java/com/example/timetable/model/repository/TimeTableRepository.kt
+++ app/src/main/java/com/example/timetable/model/repository/TimeTableRepository.kt
@@ -1,4 +1,9 @@
 package com.example.timetable.model.repository
 
+import com.example.timetable.model.source.Subject
+import kotlinx.coroutines.flow.Flow
+
 interface TimeTableRepository {
+    suspend fun addSubject(subject: Subject)
+    fun getSubjects(): Flow<List<Subject>>
 }
--- app/src/main/java/com/example/timetable/model/repository/TimeTableRepositoryImpl.kt
+++ app/src/main/java/com/example/timetable/model/repository/TimeTableRepositoryImpl.kt
@@ -1,10 +1,15 @@
 package com.example.timetable.model.repository
 
+import com.example.timetable.model.source.Subject
 import com.example.timetable.model.source.TimeTableDao
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class TimeTableRepositoryImpl @Inject constructor(
     private val timeTableDao: TimeTableDao,
 ): TimeTableRepository {
+    override suspend fun addSubject(subject: Subject) = timeTableDao.insertSubject(subject)
+
+    override fun getSubjects(): Flow<List<Subject>> = timeTableDao.getAllSubjects()
 
 }
