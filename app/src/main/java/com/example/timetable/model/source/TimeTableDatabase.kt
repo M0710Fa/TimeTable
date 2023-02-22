79ac712b06da060a7ccbab90bcbf3377f208d0b1
M0710Fa <b1018188.fun@gmail.com>
2023-02-22 18:54:00 +0900

    exportSchemaのエラーを防ぐ

--- app/src/main/java/com/example/timetable/model/source/TimeTableDatabase.kt
+++ app/src/main/java/com/example/timetable/model/source/TimeTableDatabase.kt
@@ -3,7 +3,7 @@ package com.example.timetable.model.source
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Subject::class], version = 1)
+@Database(entities = [Subject::class], version = 1, exportSchema = false)
 abstract class TimeTableDatabase: RoomDatabase() {
     abstract fun timeTableDao() : TimeTableDao
 }
