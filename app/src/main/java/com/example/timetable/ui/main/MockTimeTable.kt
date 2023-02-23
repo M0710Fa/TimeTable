ed56b4297d788e61933cb178c0adfce2bb25f622
M0710Fa <b1018188.fun@gmail.com>
2023-02-23 22:06:38 +0900

    テストのためのモックデータを作成

---
+++ app/src/main/java/com/example/timetable/ui/main/MockTimeTable.kt
@@ -0,0 +1,59 @@
+package com.example.timetable.ui.main
+
+import com.example.timetable.model.source.DailyTables
+import com.example.timetable.model.source.Subject
+
+object MockTimeTable {
+    val timetable = listOf(
+        DailyTables(
+            week = "月",
+            subjects = listOf(
+                Subject(subjectName = "1", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "2", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "3", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "4", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "5", classRoom = null, teacher = null, subjectColor = ""),
+            ),
+        ),
+        DailyTables(
+            week = "火",
+            subjects = listOf(
+                Subject(subjectName = "1", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "2", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "3", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "4", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "5", classRoom = null, teacher = null, subjectColor = ""),
+            ),
+        ),
+        DailyTables(
+            week = "水",
+            subjects = listOf(
+                Subject(subjectName = "1", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "2", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "3", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "4", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "5", classRoom = null, teacher = null, subjectColor = ""),
+            ),
+        ),
+        DailyTables(
+            week = "木",
+            subjects = listOf(
+                Subject(subjectName = "1", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "2", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "3", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "4", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "5", classRoom = null, teacher = null, subjectColor = ""),
+            ),
+        ),
+        DailyTables(
+            week = "金",
+            subjects = listOf(
+                Subject(subjectName = "1", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "2", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "3", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "4", classRoom = null, teacher = null, subjectColor = ""),
+                Subject(subjectName = "5", classRoom = null, teacher = null, subjectColor = ""),
+            ),
+        ),
+    )
+}
