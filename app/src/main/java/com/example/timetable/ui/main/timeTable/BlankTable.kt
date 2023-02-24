47cfb31dc6da0150e47c58f5ae7e797086d6a110
M0710Fa <b1018188.fun@gmail.com>
2023-02-25 00:42:55 +0900

    曜日を中央揃え

--- app/src/main/java/com/example/timetable/ui/main/timeTable/BlankTable.kt
+++ app/src/main/java/com/example/timetable/ui/main/timeTable/BlankTable.kt
@@ -14,9 +14,9 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.timetable.model.source.DailyTables
@@ -41,13 +41,16 @@ fun BlankTable(
             val tableWeight = 1f
             Spacer(modifier = modifier.width(timesWidth))
             timeTable.forEach { daily ->
-                Text(
-                    text = daily.week,
+                Box(
                     modifier = modifier
                         .weight(tableWeight)
                         .height(weeksHeight),
-                    textAlign = TextAlign.Center,
-                )
+                ) {
+                    Text(
+                        text = daily.week,
+                        modifier = modifier.align(Alignment.Center)
+                    )
+                }
             }
         }
 
