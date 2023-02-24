cb24efd963be25aafc362e0c0cff68e9170f6876
M0710Fa <b1018188.fun@gmail.com>
2023-02-25 01:02:56 +0900

    科目内容を中央揃え

--- app/src/main/java/com/example/timetable/ui/main/timeTable/TimeTable.kt
+++ app/src/main/java/com/example/timetable/ui/main/timeTable/TimeTable.kt
@@ -5,16 +5,20 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.timetable.model.source.DailyTables
@@ -30,22 +34,22 @@ fun TimeTable(
     Column() {
         Spacer(
             modifier = modifier
-                .fillMaxWidth().height(weeksHeight)
+                .fillMaxWidth().height(weeksHeight),
         )
 
         // Display TimeTable
         Row(
             modifier = modifier
-                .fillMaxWidth()
+                .fillMaxWidth(),
         ) {
             Spacer(modifier = modifier.width(timesWidth))
             Row(
                 modifier = modifier.background(
                     color = Color.Transparent,
-                )
+                ),
             ) {
                 timeTable.forEach { daily ->
-                    Box(modifier = modifier.weight(.1f)){
+                    Box(modifier = modifier.weight(.1f)) {
                         DailyColumn(dailySubject = daily.subjects)
                     }
                 }
@@ -63,9 +67,12 @@ fun DailyColumn(
         modifier = modifier,
     ) {
         dailySubject.forEach {
-            it.value.let {subject ->
-                if (subject != null)SubjectCard(subject = subject, modifier = modifier.weight(.1f))
-                else Spacer(modifier = modifier.weight(.1f))
+            it.value.let { subject ->
+                if (subject != null) {
+                    SubjectCard(subject = subject, modifier = modifier.weight(.1f))
+                } else {
+                    Spacer(modifier = modifier.weight(.1f))
+                }
             }
         }
     }
@@ -80,15 +87,29 @@ fun SubjectCard(
         modifier = modifier
             .fillMaxWidth()
             .padding(2.dp),
-        shape = RoundedCornerShape(4.dp)
+        shape = RoundedCornerShape(4.dp),
     ) {
         Column(
             modifier = modifier
+                .fillMaxSize()
                 .padding(4.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = subject.subjectName)
-            Text(text = subject.classRoom ?: "")
-            Text(text = subject.teacher ?: "")
+            Text(
+                text = subject.subjectName,
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                text = subject.classRoom ?: "",
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                text = subject.teacher ?: "",
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Center,
+            )
         }
     }
 }
