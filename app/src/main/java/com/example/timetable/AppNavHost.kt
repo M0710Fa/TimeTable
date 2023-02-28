549712fe9988599068b14d0d3fcc040b302c256c
M0710Fa <b1018188.fun@gmail.com>
2023-03-01 01:24:18 +0900

    [add]ナビゲーション用のホストを作成

---
+++ app/src/main/java/com/example/timetable/AppNavHost.kt
@@ -0,0 +1,29 @@
+package com.example.timetable
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.timetable.ui.addSubject.AddSubjectScreen
+import com.example.timetable.ui.main.MainScreen
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    startDestination: String,
+) {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ){
+        composable(route = Destinations.TimeTableScreen.route){
+            MainScreen()
+        }
+        composable(route = Destinations.SubjectsScreen.route){
+            AddSubjectScreen()
+        }
+    }
+}
