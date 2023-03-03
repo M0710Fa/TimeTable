f0e4109dc75a1b3a6bc3ad9d97c382c2028f79ce
M0710Fa <b1018188.fun@gmail.com>
2023-03-03 18:05:48 +0900

    navhostを変更

--- app/src/main/java/com/example/timetable/AppNavHost.kt
+++ app/src/main/java/com/example/timetable/AppNavHost.kt
@@ -1,12 +1,13 @@
 package com.example.timetable
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.timetable.ui.addSubject.AddSubjectScreen
-import com.example.timetable.ui.main.MainScreen
+import com.example.timetable.ui.table.TableScreen
 
 @Composable
 fun AppNavHost(
@@ -14,16 +15,19 @@ fun AppNavHost(
     startDestination: String,
 ) {
     val navController = rememberNavController()
+    Column() {
+        NavHost(
+            navController = navController,
+            startDestination = startDestination,
+        ) {
+            composable(route = Destinations.TableScreen.route) {
+                TableScreen()
+            }
+            composable(route = Destinations.SubjectsScreen.route) {
+                AddSubjectScreen()
+            }
+        }
 
-    NavHost(
-        navController = navController,
-        startDestination = startDestination,
-    ) {
-        composable(route = Destinations.TimeTableScreen.route) {
-            MainScreen()
-        }
-        composable(route = Destinations.SubjectsScreen.route) {
-            AddSubjectScreen()
-        }
+
     }
 }
