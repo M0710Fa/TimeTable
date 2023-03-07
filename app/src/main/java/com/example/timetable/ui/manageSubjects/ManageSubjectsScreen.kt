c3d22295a9e713ea97dce4d26b4db5c2915afc4b
M0710Fa <b1018188.fun@gmail.com>
2023-03-07 11:42:55 +0900

    科目管理画面に表示用コンポーザブルを追加

--- app/src/main/java/com/example/timetable/ui/manageSubjects/ManageSubjectsScreen.kt
+++ app/src/main/java/com/example/timetable/ui/manageSubjects/ManageSubjectsScreen.kt
@@ -1,11 +1,47 @@
 package com.example.timetable.ui.manageSubjects
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import java.lang.reflect.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.timetable.model.source.Subject
+import com.example.timetable.ui.addSubject.SubjectContent
 
 @Composable
 fun ManageSubjectsScreen(
-    modifier: Modifier
+    modifier: Modifier = Modifier,
+    viewModel: ManageSubjectsViewModel = hiltViewModel(),
 ) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    DisplaySubjects(subjects = uiState.subjects)
+}
 
+@Composable
+fun DisplaySubjects(
+    modifier: Modifier = Modifier,
+    subjects: List<Subject>,
+) {
+    Column(
+        modifier = modifier.padding(8.dp),
+    ) {
+        Text(
+            text = "登録された科目一覧",
+            style = MaterialTheme.typography.titleLarge,
+        )
+        LazyColumn(
+            modifier = modifier.padding(8.dp),
+        ) {
+            items(subjects) {
+                SubjectContent(subject = it)
+            }
+        }
+    }
 }
