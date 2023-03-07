1919e2cb62f5688cb8c82d8ef8a82a1a88153108
M0710Fa <b1018188.fun@gmail.com>
2023-03-07 11:35:18 +0900

    科目管理用スクリーンを置くためのファイルの作成

---
+++ app/src/main/java/com/example/timetable/ui/manageSubjects/ManageSubjectsScreen.kt
@@ -0,0 +1,11 @@
+package com.example.timetable.ui.manageSubjects
+
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun ManageSubjectsScreen(
+    modifier: Modifier
+) {
+
+}
