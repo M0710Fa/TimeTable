186e1ccc43349c0561bb0e76910bd2068b5de034
M0710Fa <b1018188.fun@gmail.com>
2023-03-10 08:23:39 +0900

    科目追加画面に遷移した際に最初のテキストフィールドにフォーカスを置く

--- app/src/main/java/com/example/timetable/ui/addSubject/AddSubjectScreen.kt
+++ app/src/main/java/com/example/timetable/ui/addSubject/AddSubjectScreen.kt
@@ -16,9 +16,13 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -85,6 +89,9 @@ fun AddSubjectContent(
     updateClassRoom: (String) -> Unit,
     updateTeacher: (String) -> Unit,
 ) {
+
+    val focusRequester = remember { FocusRequester() }
+
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -104,7 +111,7 @@ fun AddSubjectContent(
             label = {
                 Text(text = stringResource(id = R.string.subject_name))
             },
-            modifier = textFieldModifier,
+            modifier = textFieldModifier.focusRequester(focusRequester),
         )
         OutlinedTextField(
             value = classRoom,
@@ -122,6 +129,10 @@ fun AddSubjectContent(
             },
             modifier = textFieldModifier,
         )
+    }
+
+    LaunchedEffect(Unit){
+        focusRequester.requestFocus()
     }
 }
 
