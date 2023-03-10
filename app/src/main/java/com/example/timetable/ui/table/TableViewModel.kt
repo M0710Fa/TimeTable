5d0f73e20da7e99a755ce86462918f0c7397feb1
M0710Fa <b1018188.fun@gmail.com>
2023-03-10 22:14:55 +0900

    時間割用のViewModelを追加

---
+++ app/src/main/java/com/example/timetable/ui/table/TableViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.timetable.ui.table
+
+import androidx.lifecycle.ViewModel
+import com.example.timetable.model.repository.TimeTableRepository
+import com.example.timetable.model.source.DailyTables
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+data class TableUiState(
+    val subjectsTable: List<DailyTables> = emptyList(),
+    val weeks : Int = 0,
+    val times : Int = 0,
+)
+
+@HiltViewModel
+class TableViewModel @Inject constructor(
+    private val timeTableRepository: TimeTableRepository
+): ViewModel() {
+    private val _uiState = MutableStateFlow(TableUiState())
+    val uiState: StateFlow<TableUiState> = _uiState
+
+    init{
+        getWeeksAndTimes()
+    }
+
+    fun getWeeksAndTimes(){
+        _uiState.update {
+            it.copy(weeks = 5, times = 5)
+        }
+    }
+}
