b9f24308264b5e97023ff77ce7e7b89319d9d16a
M0710Fa <b1018188.fun@gmail.com>
2023-03-10 22:15:47 +0900

    増えた引数を追加

--- app/src/main/java/com/example/timetable/ui/table/TableScreen.kt
+++ app/src/main/java/com/example/timetable/ui/table/TableScreen.kt
@@ -2,20 +2,26 @@ package com.example.timetable.ui.table
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.timetable.ui.table.timeTable.BlankTable
 import com.example.timetable.ui.table.timeTable.TimeTable
 
 @Composable
 fun TableScreen(
     modifier: Modifier = Modifier,
+    viewModel: TableViewModel = hiltViewModel()
 ) {
     val weeksHeight = 32.dp
     val timesWidth = 24.dp
 
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
     Box() {
-        BlankTable(timeTable = MockTimeTable.timetable, weeksHeight = weeksHeight, timesWidth = timesWidth)
-        TimeTable(timeTable = MockTimeTable.timetable, weeksHeight = weeksHeight, timesWidth = timesWidth)
+        BlankTable(timeTable = MockTimeTable.timetable, weeksHeight = weeksHeight, timesWidth = timesWidth, weeks = uiState.weeks, times = uiState.times)
+        TimeTable(timeTable = MockTimeTable.timetable, weeksHeight = weeksHeight, timesWidth = timesWidth, weeks = uiState.weeks, times = uiState.times)
     }
 }
