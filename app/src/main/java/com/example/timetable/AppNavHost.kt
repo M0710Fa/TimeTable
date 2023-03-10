774ea2e1adc6508fc89d2050065828ed8793861f
M0710Fa <b1018188.fun@gmail.com>
2023-03-10 09:49:31 +0900

    アニメーションの修正

--- app/src/main/java/com/example/timetable/AppNavHost.kt
+++ app/src/main/java/com/example/timetable/AppNavHost.kt
@@ -84,10 +84,10 @@ fun AppNavHost(
             composable(
                 route = Destinations.AddSubjectScreen.route,
                 enterTransition = {
-                    slideInVertically(initialOffsetY = { fullHeight -> fullHeight })
+                    slideInVertically(initialOffsetY = { fullHeight -> fullHeight-100 })
                 },
                 exitTransition = {
-                    slideOutVertically(targetOffsetY = { fullHeight -> fullHeight })
+                    slideOutVertically(targetOffsetY = { fullHeight -> fullHeight-100 })
                 },
             ) {
                 AddSubjectScreen(
