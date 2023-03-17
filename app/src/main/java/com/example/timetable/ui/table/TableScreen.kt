83b3baedc545cfd5ffcb6617152731009f9feb24
M0710Fa <b1018188.fun@gmail.com>
2023-03-17 22:19:01 +0900

    科目を選択するためのスクリーンを作成

--- app/src/main/java/com/example/timetable/ui/table/TableScreen.kt
+++ app/src/main/java/com/example/timetable/ui/table/TableScreen.kt
@@ -7,13 +7,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavController
 import com.example.timetable.ui.table.timeTable.BlankTable
 import com.example.timetable.ui.table.timeTable.TimeTable
 
 @Composable
 fun TableScreen(
     modifier: Modifier = Modifier,
-    viewModel: TableViewModel = hiltViewModel()
+    viewModel: TableViewModel = hiltViewModel(),
+    navController: NavController,
 ) {
     val weeksHeight = 32.dp
     val timesWidth = 24.dp
@@ -21,7 +23,20 @@ fun TableScreen(
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     Box() {
-        BlankTable(timeTable = MockTimeTable.timetable, weeksHeight = weeksHeight, timesWidth = timesWidth, weeks = uiState.weeks, times = uiState.times)
-        TimeTable(timeTable = MockTimeTable.timetable, weeksHeight = weeksHeight, timesWidth = timesWidth, weeks = uiState.weeks, times = uiState.times)
+        BlankTable(
+            timeTable = MockTimeTable.timetable,
+            weeksHeight = weeksHeight,
+            timesWidth = timesWidth,
+            weeks = uiState.weeks,
+            times = uiState.times
+        )
+        TimeTable(
+            timeTable = MockTimeTable.timetable,
+            weeksHeight = weeksHeight,
+            timesWidth = timesWidth,
+            weeks = uiState.weeks,
+            times = uiState.times,
+            navController = navController
+        )
     }
 }
